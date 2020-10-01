3e72b37f5d4c5ba646b3fc1fea0a11fb44d09104
Weston McNamara <westonfmcnamara@gmail.com>
2020-10-01 14:55:33 -0400

    Edited AABB collision function slightly

--- src/Core/Collision/AABB.cpp
+++ src/Core/Collision/AABB.cpp
@@ -3,20 +3,20 @@ namespace Crynn
 {
     bool AABBSolver::Compare(const AABB& one, const AABB& two)
     {
-        // collision x-axis?
-        bool collisionX = one.transform.GetPosition().x + one.Size.x >= two.transform.GetPosition().x &&
+        //x collision
+        bool x = one.transform.GetPosition().x + one.Size.x >= two.transform.GetPosition().x &&
             two.transform.GetPosition().x + two.Size.x >= one.transform.GetPosition().x;
 
-        // collision y-axis?
-        bool collisionY = one.transform.GetPosition().y + one.Size.y >= two.transform.GetPosition().y &&
+        //y collision
+        bool y = one.transform.GetPosition().y + one.Size.y >= two.transform.GetPosition().y &&
             two.transform.GetPosition().y + two.Size.y >= one.transform.GetPosition().y;
 
-        // collision z-axis?
-        bool collisionZ = one.transform.GetPosition().z + one.Size.z >= two.transform.GetPosition().z &&
+        //z collision
+        bool z = one.transform.GetPosition().z + one.Size.z >= two.transform.GetPosition().z &&
             two.transform.GetPosition().z + two.Size.z >= one.transform.GetPosition().z;
 
         // collision only if on both axes
-        return collisionX && collisionY && collisionZ;
+        return x && y && z;
     }
 
     bool AABBSolver::Compare(const AABB& one, const AABB& two, std::function<void(const AABB& one, const AABB& two)> func)
