5a9278b91afb935bb91f664c41360e58b6f94bdb
Weston McNamara <westonfmcnamara@gmail.com>
2021-02-16 23:23:54 -0500

    Amended Example.cpp to reflect changes

--- src/Example.cpp
+++ src/Example.cpp
@@ -7,13 +7,13 @@ using namespace crynn;
 int main()
 {
 	Window window("Crynn", 800, 600); //Initialise crynn, and the window
-
 	Application::Initialise(); //Calls OnStart events. Should be right before the while loop.
+
 	while (!window.ShouldClose())
 	{
-		window.BeforeRender(); //Clears buffers and sets up GUI data
-		Application::Tick(); //Ticks update events.
-		window.AfterRender(); //Renders GUI and swaps buffers.
+		window.BeforeRender();
+		Application::Tick();
+		window.AfterRender();
 	}
 
 	return 0;
