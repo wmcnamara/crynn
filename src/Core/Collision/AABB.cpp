4fb4b1b75e6371c4a6334a98ee6d09386be3da29
Weston McNamara <westonfmcnamara@gmail.com>
2021-02-20 17:29:12 -0500

    Slightly optmised AABB comparison to prevent uneccesarry calculations

--- src/Core/Collision/AABB.cpp
+++ src/Core/Collision/AABB.cpp
@@ -7,16 +7,25 @@ namespace crynn
         bool x = one.transform.GetPosition().x + one.Size.x >= two.transform.GetPosition().x &&
             two.transform.GetPosition().x + two.Size.x >= one.transform.GetPosition().x;
 
+        if (!x)
+            return false;
+
         //y collision
         bool y = one.transform.GetPosition().y + one.Size.y >= two.transform.GetPosition().y &&
             two.transform.GetPosition().y + two.Size.y >= one.transform.GetPosition().y;
+
+        if (!y)
+            return false;
 
         //z collision
         bool z = one.transform.GetPosition().z + one.Size.z >= two.transform.GetPosition().z &&
             two.transform.GetPosition().z + two.Size.z >= one.transform.GetPosition().z;
 
+        if (!z)
+            return false;
+
         // collision only if on every axes
-        return x && y && z;
+        return true;
     }
 
     bool AABBSolver::Compare(const AABB& one, const AABB& two, std::function<void(const AABB& one, const AABB& two)> func)
