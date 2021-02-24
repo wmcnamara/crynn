14392a6253fc015bbbb4448c505312c8aff61b56
Weston McNamara <westonfmcnamara@gmail.com>
2021-02-24 15:34:58 -0500

    Added clamp function

--- src/Core/Math/Math.cpp
+++ src/Core/Math/Math.cpp
@@ -4,3 +4,14 @@ float Math::Lerp(float min, float max, float t)
 {
 	return ((max - min) * t) + min;
 }
+
+float Math::Clamp(float val, float min, float max)
+{
+	if (val > max)
+		return max;
+
+	if (val < min)
+		return min;
+
+	return val;
+}
--- src/Core/Math/Math.h
+++ src/Core/Math/Math.h
@@ -3,5 +3,6 @@ class Math
 {
 public:
 	float Lerp(float min, float max, float t);
+	float Clamp(float val, float min, float max);
 };
 
