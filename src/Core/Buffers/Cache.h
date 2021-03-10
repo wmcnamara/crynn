67c8fad7825c77cd42e63d5f3486665e0c46b8f8
Weston McNamara <westonfmcnamara@gmail.com>
2021-03-10 10:46:21 -0500

    Improved Cache.h

--- src/Core/Buffers/Cache.h
+++ src/Core/Buffers/Cache.h
@@ -1,5 +1,6 @@
 #pragma once
 #include <utility>
+#include <functional>
 
 namespace crynn
 {
@@ -7,28 +8,34 @@ namespace crynn
 	/// Encapsulates a cache for an object. 
 	/// Provides useful functions for checking the validity of a cache, and if it needs to be recalculated.
 	/// </summary>
-	/// <typeparam name="T">Object to be cached</typeparam>
 	template<typename T>
 	class Cache
 	{
+		//Constructs a cache object.
+		Cache(T& obj, std::function<void(T&)> recalculate) : m_recalculate(recalculate)
+
 		inline void Invalidate() { valid = false; }
 		inline void Validate() { valid = true; }
 
 		inline bool IsValid() const { return valid; }
-
-		void Set(T&& _data) { data = std::move(_data); }
+		
 		void Set(T& _data) { data = std::move(_data); }
 
+		//Returns a constant reference to the data. 
+		//Does not invalidate the cache.
 		const T& GetData() { return data; }
 
-		T& GetData() 
+		//Returns a non const reference to the data.
+		//Invalidates the cache.
+		T& GetDataNonConst() 
 		{ 
 			Invalidate(); //when returning a non const reference, invalidate.
 			return data;
 		}
 
 	private:
-		T data;
-		bool valid = true;
+		T& data;
+		std::function<void(T&)> m_recalculate;
+		bool valid = false;
 	};
 }
