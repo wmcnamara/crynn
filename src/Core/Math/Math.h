c16b1b68bf03ee50e57476ddd2f3e08f19c2cc52
Weston McNamara <westonfmcnamara@gmail.com>
2021-03-17 11:42:17 -0400

    Made math functions constexpr

--- src/Core/Math/Math.h
+++ src/Core/Math/Math.h
@@ -6,15 +6,15 @@ class Math
 {
 public:
 	//Performs a linear interpolation of min and max with t.
-	static float Lerp(float min, float max, float t);
+	static constexpr float Lerp(float min, float max, float t);
 	 
 	//Performs a linear interpolation of min and max with t. This function clamps t to 0 - 1.
-	static float LerpClamped(float min, float max, float t);
+	static constexpr float LerpClamped(float min, float max, float t);
 	 
 	//Returns val clamped between min and max.
-	static float Clamp(float min, float max, float val);
+	static constexpr float Clamp(float min, float max, float val);
 	 
 	//Returns a bool indicating if n is between min and max.
-	static bool Between(float min, float max, float n);
+	static constexpr bool Between(float min, float max, float n);
 };
 
