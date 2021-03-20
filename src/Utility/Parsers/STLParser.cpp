b6f79fa8c3adad8cafc8ece662f098de08e9beaf
Weston McNamara <westonfmcnamara@gmail.com>
2021-03-20 15:32:17 -0400

    Simplified STLParser and added ComputeGLFormat

--- src/Utility/Parsers/STLParser.cpp
+++ src/Utility/Parsers/STLParser.cpp
@@ -30,25 +30,67 @@ namespace crynn
 				//Parse vertex
 				lineStart += sizeof("vertex"); //offset pointer to the floating point data
 
-				Vec3 vertex = Vec3::Parse(lineStart);
+				//Get vector components
+				float x = 0.0f, y = 0.0f, z = 0.0f;
+				char* nextFloatEnd; //used in strtof. check strtof docs to understand
 
-				stlData->vertices.push_back(vertex.x);
-				stlData->vertices.push_back(vertex.y);
-				stlData->vertices.push_back(vertex.z);
+				//Parse the vertex values
+				x = strtof(lineStart, &nextFloatEnd);
+				y = strtof(nextFloatEnd, &nextFloatEnd);
+				z = strtof(nextFloatEnd, NULL);
+
+				stlData->vertices.push_back(x);
+				stlData->vertices.push_back(y);
+				stlData->vertices.push_back(z);
 			}
 			else if (strstr(line.c_str(), "facet normal") != NULL)
 			{
 				//Parse normal vector
 				lineStart += sizeof("facet normal"); //offset pointer to the floating point data
 
-				Vec3 normal = Vec3::Parse(lineStart);
+				//Get vector components
+				float x = 0.0f, y = 0.0f, z = 0.0f;
+				char* nextFloatEnd; //used in strtof. check strtof docs to understand
 
-				stlData->normals.push_back(normal.x);
-				stlData->normals.push_back(normal.y);
-				stlData->normals.push_back(normal.z);
+				//Parse the vertex values
+				x = strtof(lineStart, &nextFloatEnd);
+				y = strtof(nextFloatEnd, &nextFloatEnd);
+				z = strtof(nextFloatEnd, NULL);
+
+				stlData->normals.push_back(x);
+				stlData->normals.push_back(y);
+				stlData->normals.push_back(z);
 			}
 		}
 
 		return stlData;
+	}
+
+	std::vector<GLfloat> STLData::ComputeGLFormat()
+	{
+		std::vector<GLfloat> vertData;
+		int normIter = 0;
+		int vertIter = 0;
+
+
+		for (int i = 0; i < vertices.size() / 3; i++)
+		{
+			vertData.push_back(vertices[vertIter]);
+			vertData.push_back(vertices[vertIter +1]);
+			vertData.push_back(vertices[vertIter +2]);
+
+			vertData.push_back(normals[normIter]);
+			vertData.push_back(normals[normIter + 1]);
+			vertData.push_back(normals[normIter + 2]);
+
+			std::cout << "norm " << "x: " << normals[normIter] << " y: " << normals[normIter + 1] << " z: " << normals[normIter + 2] << "\n";
+			//Every three vertices get a new normal vector
+			if (i % 3 == 0 && i != 0)
+				normIter +=3;
+
+			vertIter += 3;
+		}	
+
+		return vertData;
 	}
 }
--- src/Utility/Parsers/STLParser.h
+++ src/Utility/Parsers/STLParser.h
@@ -1,6 +1,5 @@
 #pragma once
 #include "../IO.h"
-#include "../../Core/Math/Vec3.h"
 
 namespace crynn
 {
@@ -13,15 +12,34 @@ namespace crynn
 		//Returns a boolean indicating if this STLData object contains no data.
 		bool Empty() const { return vertices.empty() && normals.empty(); }
 
+		//Returns the number of vertex elements loaded from the model. 
 		size_t VertexCount() const { return vertices.size(); }
+
+		//Returns the number of normal vector elements loaded from the model.
 		size_t NormalCount() const { return normals.size(); }
 
 		//Returns the vertices in an OpenGL supported format.
-		//This function is relatively slow and should only be called when constructing objects.
 		GLfloat* GetVertices() { return vertices.data(); }
 
+		//Returns the normals in an OpenGL supported format.
+		GLfloat* GetNormals() { return normals.data(); }
+		
+		/*
+		//Computes an array that can be passed to a mesh with the vertices and normals aligned correctly.
+		//Combines vertices and normals together
+
+		//Data is aligned as such:
+		//vertex x, y, z, normal x, y, z, etc.
+		//Example vertex memory layout: 1.0f, 2.0f, 1.0f, 0.5f, 0.5f, 0.5f, 
+		//6 floats per vertex
+		*/
+		std::vector<GLfloat> ComputeGLFormat();
+
+	private:
 		std::vector<float> vertices;
 		std::vector<float> normals;
+
+		friend class STLParser;
 	};
 
 	using STLDataPtr = std::shared_ptr<STLData>;
