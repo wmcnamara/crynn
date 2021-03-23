e1d45aa9ced3d95685d665b7767e9ef82dbb385c
Weston McNamara <westonfmcnamara@gmail.com>
2021-03-22 22:56:32 -0400

    Added Defines.h

---
+++ src/Utility/Defines.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#define NODISCARD [[nodiscard]]
+#define DLLEXPORT extern "c" __declspec(dllexport)
+
+#ifdef _DEBUG 
+#define CRYNN_DEBUG
+#elif
+#define CRYNN_RELEASE
+#endif
+
+#ifdef _WIN32 
+#define CRYNN_WINDOWS
+#endif
+
+#ifdef _WIN64
+#define CRYNN_WINDOWS
+#endif
