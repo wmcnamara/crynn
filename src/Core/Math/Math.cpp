0bab4ad82b50e77fcd97e487a8eda4918a7f6ae1
Weston McNamara <westonfmcnamara@gmail.com>
2021-03-26 18:43:46 -0400

    added math typedefs and vector functions to math class. Removed crynn vector classes

--- src/Core/Math/Math.cpp
+++ src/Core/Math/Math.cpp
@@ -1,1 +1,33 @@
 #include "Math.h"
+
+namespace crynn
+{
+	Vec3 crynn::ParseVec3(const char* textStart)
+	{
+		//Get vector components
+		float x = 0.0f, y = 0.0f, z = 0.0f;
+		char* nextFloatEnd; //used in strtof. check strtof docs to understand
+
+		//Parse the vertex values
+		x = strtof(textStart, &nextFloatEnd);
+		y = strtof(nextFloatEnd, &nextFloatEnd);
+		z = strtof(nextFloatEnd, NULL);
+
+		//Create Vec3
+		return Vec3(x, y, z);
+	}
+
+	Vec2 crynn::ParseVec2(const char* textStart)
+	{
+		//Get vector components
+		float x = 0.0f, y = 0.0f, z = 0.0f;
+		char* nextFloatEnd; //used in strtof. check strtof docs to understand
+
+		//Parse the vertex values
+		x = (int)strtof(textStart, &nextFloatEnd);
+		y = (int)strtof(nextFloatEnd, NULL);
+
+		//Create Vec3
+		return Vec2(x, y);
+	}
+}
--- src/Core/Math/Math.h
+++ src/Core/Math/Math.h
@@ -1,39 +1,71 @@
 #pragma once
+#include <glm/glm.hpp>
 
-/// <summary>
-/// Collection of floating point math functions
-/// </summary>
-class Math
-{
-public:
-	//Performs a linear interpolation of min and max with t.
-	static constexpr float Lerp(float min, float max, float t)
-	{
-		return ((max - min) * t) + min;
-	}
-	 
-	//Performs a linear interpolation of min and max with t. This function clamps t to 0 - 1.
-	static constexpr float LerpClamped(float min, float max, float t)
-	{
-		Clamp(0.0f, 1.0f, t);
-		return ((max - min) * t) + min;
-	}
-	 
-	//Returns val clamped between min and max.
-	static constexpr float Clamp(float min, float max, float val)
-	{
-		if (val > max)
-			return max;
+namespace crynn
+{		
+	//GLM Math typedefs
+	using Vec2 = glm::vec2;
+	using Vec3 = glm::vec3;
+	using Vec4 = glm::vec4;
 
-		if (val < min)
-			return min;
+	using Mat3 = glm::mat3;
+	using Mat4 = glm::mat4;
 
-		return val;
-	}
-	 
-	//Returns a bool indicating if n is between min and max.
-	static constexpr bool Between(float min, float max, float n)
+	using Vec3Int = glm::ivec3;
+	using Vec2Int = glm::ivec2;
+
+
+	/*
+		Creates a glm::vec3 from a string starting at parameter textStart.
+		Expected in the general format: "-1.0 1.0 1.0" for Vec3(-1.0, 1.0, 1.0), or something similar.
+		Should not have other text inside of it. Only the numbers, and spaces between them.
+		Will place a value of 0 for any component that fails to parse.
+	*/	
+	static Vec3 ParseVec3(const char* textStart);
+
+	/*
+		Creates a glm::vec2 from a string starting at parameter textStart.
+		Expected in the general format: "-1.0 1.0" for Vec2(-1.0, 1.0) or something similar.
+		Should not have other text inside of it. Only the numbers, and spaces between them.
+		Will place a value of 0 for any component that fails to parse.
+	*/
+	static Vec2 ParseVec2(const char* textStart);
+
+	/// <summary>
+	/// Collection of floating point math functions
+	/// </summary>
+	class Math
 	{
-		return n < max && n > min;
-	}
-};
+	public:
+		//Performs a linear interpolation of min and max with t.
+		static constexpr float Lerp(float min, float max, float t)
+		{
+			return ((max - min) * t) + min;
+		}
+
+		//Performs a linear interpolation of min and max with t. This function clamps t to 0 - 1.
+		static constexpr float LerpClamped(float min, float max, float t)
+		{
+			Clamp(0.0f, 1.0f, t);
+			return ((max - min) * t) + min;
+		}
+
+		//Returns val clamped between min and max.
+		static constexpr float Clamp(float min, float max, float val)
+		{
+			if (val > max)
+				return max;
+
+			if (val < min)
+				return min;
+
+			return val;
+		}
+
+		//Returns a bool indicating if n is between min and max.
+		static constexpr bool Between(float min, float max, float n)
+		{
+			return n < max&& n > min;
+		}
+	};
+}
