94eb9322b48e9153908b58da391372d5af1f21bc
Weston McNamara <westonfmcnamara@gmail.com>
2021-04-10 19:37:00 -0400

    Added AddBox to rigidbody and updated project files

--- src/Core/Physics/Rigidbody.cpp
+++ src/Core/Physics/Rigidbody.cpp
@@ -1,7 +1,7 @@
 #include "Rigidbody.h"
 namespace crynn
 {
-	Rigidbody::Rigidbody(RigidbodyDef def)
+	Rigidbody::Rigidbody(RigidbodyDef def, Transform& transform) : m_transform(transform)
 	{
 		assert(Physics::IsInit() && "Please initialise the physics engine with Physics::Init()");
 
@@ -11,5 +11,17 @@ namespace crynn
 	Rigidbody::~Rigidbody()
 	{
 		Physics::GetScene()->RemoveBody(body);
+	}
+
+	void Rigidbody::AddBox(Vec3 offset, Mat3 rotation, Vec3 extents)
+	{
+		BoxDef def;
+		q3Transform tx;
+
+		tx.position = offset;
+		tx.rotation = rotation;
+		def.Set(tx, extents);
+
+		body->AddBox(def);
 	}
 }
--- src/Core/Physics/Rigidbody.h
+++ src/Core/Physics/Rigidbody.h
@@ -1,20 +1,25 @@
 #pragma once
 #include "Physics.h"
 
-
-//Document this
-
 namespace crynn
 {
 	using RigidbodyDef = q3BodyDef;
+	using BoxDef = q3BoxDef;
 
 	class Rigidbody
 	{
 	public:
-		Rigidbody(RigidbodyDef def);
-		virtual ~Rigidbody();
+		//Construct a rigidbody. transform is the transform of the object you would like to be simulated. It also acts as default values.
+		Rigidbody(RigidbodyDef def, Transform& transform);
+ 		virtual ~Rigidbody();
 
+		//Adds a box collider to the rigidbody.
+		//Offset is a positional offset from the center of the rigidbody. This is not in worldspace, it is local to the rigidbody, and likely the object.
+		//Extents is the length width and height of the box
+		//Rotation is the rotation matrix you would like to set as the default rotation.
+		void AddBox(Vec3 offset, Mat3 rotation = Mat3(1.0f), Vec3 extents = Vec3(1.0, 1.0, 1.0));
 	private:
 		q3Body* body;
+		Transform& m_transform;
 	};
 }
