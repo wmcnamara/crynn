07d523d287852107e0cc2e7c93c87f117174ab5f
Weston McNamara <westonfmcnamara@gmail.com>
2021-04-10 18:13:35 -0400

    Updated physics class

--- src/Core/Physics/Physics.h
+++ src/Core/Physics/Physics.h
@@ -1,18 +1,26 @@
 #pragma once
-#include "../../Core/Math/Vec3.h"
 #include "../Physics/Collision/AABB.h"
+#include "qu3e/src/q3.h"
 
 namespace crynn
 {
-	class Ray 
+	/// <summary>
+	/// This class manages q3 physics scenes. It is not meant to be instantiated
+	/// </summary>
+	class Physics
 	{
 	public:
-		//Constructs a ray from a given origin, direction and length. Direction must be normalized.
-		Ray(Vec3 origin, Vec3 direction, float length);
+		Physics() = delete;
+		~Physics() = delete;
 
-		bool CompAABB(AABB& other);
+		static void Init(); //Initialises the physics engine
+		static void UnInit(); //Deinitialises the physics engine
+
+		static bool IsInit(); //Is the physics engine initialised?
+
+		static q3Scene* GetScene();
 	private:
-		Vec3 m_origin, m_dir;
-		float m_length;
+		static inline bool initialised = false;
+		static inline q3Scene* scene = nullptr;
 	};
 }
