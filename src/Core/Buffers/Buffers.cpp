b9b1727e52e6d511ef206a0bfaa8472fa088b7a6
Weston McNamara <westonfmcnamara@gmail.com>
2021-04-27 17:28:27 -0400

    Improved RAII buffer classes

--- src/Core/Buffers/Buffers.cpp
+++ src/Core/Buffers/Buffers.cpp
@@ -1,15 +1,32 @@
 #include "Buffers.h"
 
-crynn::VBO::VBO(float* vertexData, size_t size) : m_size(size)
+crynn::VBO::VBO(float* vertices, size_t numOfVertices) : m_size(numOfVertices)
 {
 	glGenBuffers(1, &ID);
 	glBindBuffer(GL_ARRAY_BUFFER, ID);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(vertexData) * size, vertexData, GL_STATIC_DRAW);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices) * numOfVertices, vertices, GL_STATIC_DRAW);
+
+	initialized = true;
 }
 
 crynn::VBO::~VBO()
 {
 	glDeleteBuffers(1, &ID);
+}
+
+void crynn::VBO::Set(float* vertices, size_t numOfVertices)
+{
+	//DO NOT call Set unless the object was constructed with the default constructor. 
+	//DO NOT call Set twice.
+	assert(!initialized);
+
+	m_size = numOfVertices;
+
+	glGenBuffers(1, &ID);
+	glBindBuffer(GL_ARRAY_BUFFER, ID);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices) * numOfVertices, vertices, GL_STATIC_DRAW);
+
+	initialized = true;
 }
 
 crynn::VAO::VAO()
@@ -23,14 +40,31 @@ crynn::VAO::~VAO()
 	glDeleteVertexArrays(1, &ID);
 }
 
-crynn::EBO::EBO(unsigned int* indices, unsigned int numOfIndices)
+crynn::EBO::EBO(unsigned int* indices, size_t numOfIndices)
 {
 	glGenBuffers(1, &ID);
 	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ID);
 	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices) * numOfIndices, indices, GL_STATIC_DRAW);
+
+	initialized = true;
 }
 
 crynn::EBO::~EBO()
 {
 	glDeleteBuffers(1, &ID);
+}
+
+void crynn::EBO::Set(unsigned int* indices, size_t numOfIndices)
+{
+	//DO NOT call Set unless the object was constructed with the default constructor. 
+	//DO NOT call Set twice.
+	assert(!initialized);
+
+	m_size = numOfIndices;
+
+	glGenBuffers(1, &ID);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ID);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices) * numOfIndices, indices, GL_STATIC_DRAW);
+
+	initialized = true;
 }
