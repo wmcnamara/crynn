8eaddd9786496f56f999f30be5ab6e056a09fb81
Weston McNamara <westonfmcnamara@gmail.com>
2021-05-06 12:48:39 -0400

    added back submod

--- lib/thirdparty/assimp/bin/contrib/zlib/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Github/crynn/lib/thirdparty/assimp/contrib/zlib
-# Build directory: C:/Github/crynn/lib/thirdparty/assimp/bin/contrib/zlib
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
