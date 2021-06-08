29a1485c94209c56c9b4b1e120e1dbd11f6039d1
Weston McNamara <westonfmcnamara@gmail.com>
2021-06-08 13:39:40 -0400

    Added unit testing project

---
+++ src/Tests/Tests.cpp
@@ -0,0 +1,19 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+#include "../Crynn.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace Tests
+{
+	TEST_CLASS(Light)
+	{
+	public:
+		
+		TEST_METHOD(DirLightCTor)
+		{
+		
+		}
+	};
+}
+1
