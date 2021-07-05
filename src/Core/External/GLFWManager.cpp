7e852e441ba3558b6226c28eb7152833294996e1
Weston McNamara <westonfmcnamara@gmail.com>
2021-07-05 00:17:14 -0400

    Added GLFWManager class and project files for shader

---
+++ src/Core/External/GLFWManager.cpp
@@ -0,0 +1,1 @@
+#include "GLFWManager.h"
---
+++ src/Core/External/GLFWManager.h
@@ -0,0 +1,7 @@
+#pragma once
+//TODO Write all GLFW function subscriptions in here and call it from the application start.
+//This will clean up the GLFW code in Window and Application
+class GLFWManager
+{
+};
+
