1b7b96f31b2faed676847c2d713e882ee2afeab1
Weston McNamara <westonfmcnamara@gmail.com>
2021-09-07 20:34:20 -0400

    Added improved GLFW init code to GLFWManager

--- src/Core/External/GLFWManager.cpp
+++ src/Core/External/GLFWManager.cpp
@@ -1,1 +1,26 @@
 #include "GLFWManager.h"
+#include <GLFW/glfw3.h>
+#include <iostream>
+
+GLFWManager::GLFWManager(int glfwMajorVersion, int glfwMinorVersion, int AASamples)
+{
+	if (glfwInit() == GLFW_TRUE) 
+	{
+		glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, glfwMajorVersion);
+		glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, glfwMinorVersion);
+		glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+		glfwWindowHint(GLFW_SAMPLES, AASamples);
+
+		std::cout << "GLFW successfully initialized...\n";
+	}
+	else
+	{
+		throw std::exception("GLFW failed to initialize!");
+	}
+}
+
+GLFWManager::~GLFWManager()
+{
+	std::cout << "Terminating GLFW\n";
+	glfwTerminate();
+}
--- src/Core/External/GLFWManager.h
+++ src/Core/External/GLFWManager.h
@@ -1,7 +1,15 @@
 #pragma once
+#include <atomic>
+
 //TODO Write all GLFW function subscriptions in here and call it from the application start.
 //This will clean up the GLFW code in Window and Application
 class GLFWManager
 {
+public:
+	GLFWManager(int glfwMajorVersion = 3, int glfwMinorVersion = 3, int AASamples = 4);
+	~GLFWManager();
+
+private:
+	static GLFWManager m_instance;
 };
 
