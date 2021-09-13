0201330923479e0fd1d052d351c1cd5e073f37c9
Weston McNamara <westonfmcnamara@gmail.com>
2021-09-13 19:57:25 -0400

    Added GLFWManager singleton

--- src/Core/External/GLFWManager.cpp
+++ src/Core/External/GLFWManager.cpp
@@ -2,15 +2,15 @@
 #include <GLFW/glfw3.h>
 #include <iostream>
 
-GLFWManager::GLFWManager(int glfwMajorVersion, int glfwMinorVersion, int AASamples)
+GLFWManager::GLFWManager()
 {
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, glfwMajorVersion);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, glfwMinorVersion);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	glfwWindowHint(GLFW_SAMPLES, AASamples);
+
 	if (glfwInit() == GLFW_TRUE) 
 	{
-		glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, glfwMajorVersion);
-		glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, glfwMinorVersion);
-		glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-		glfwWindowHint(GLFW_SAMPLES, AASamples);
-
 		std::cout << "GLFW successfully initialized...\n";
 	}
 	else
--- src/Core/External/GLFWManager.h
+++ src/Core/External/GLFWManager.h
@@ -1,15 +1,25 @@
 #pragma once
 #include <atomic>
 
+//Configuration for the opengl versioning and anti aliasing samples for GLFW
+//Change these to whatever you desire 
+
+constexpr inline int glfwMajorVersion = 3;
+constexpr inline int glfwMinorVersion = 3;
+constexpr inline int AASamples = 4;
+
 //TODO Write all GLFW function subscriptions in here and call it from the application start.
 //This will clean up the GLFW code in Window and Application
 class GLFWManager
 {
-public:
-	GLFWManager(int glfwMajorVersion = 3, int glfwMinorVersion = 3, int AASamples = 4);
-	~GLFWManager();
-
+public: 
+	static GLFWManager& Instance() 
+	{
+		static GLFWManager S;
+		return S;
+	}
 private:
-	static GLFWManager m_instance;
+	GLFWManager();
+	~GLFWManager();
 };
 
